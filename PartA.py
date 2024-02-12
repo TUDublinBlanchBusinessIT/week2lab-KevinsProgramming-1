b46357ddf7813fe36314314f763b0623984c6b53
KevinsProgramming <158289787+KevinsProgramming@users.noreply.github.com>
2024-02-12 11:17:01 +0000

    Add files via upload

---
+++ PartA.py
@@ -0,0 +1,16 @@
+#Kevin Tshiaka
+#12/02/24
+#A progress to Print Steps 1 to 9 to the screen
+
+def printstep():
+    print("Step 4")
+    print("Step 5")
+    print("Step 6")
+
+print("Step 1")
+print("Step 2")
+print("Step 3")
+printSteps()
+print("Step 7")
+print("Step 8")
+print("Step 9")
